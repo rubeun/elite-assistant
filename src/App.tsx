2d4f845eff6616b00bab55b5b59e575d6ad0d7bf
Rubeun Tan <github@rubeun.com>
2025-03-15 16:29:13 -0700

    move header to Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
-import elitelogo from './logo-elite1.svg';
 import './App.css';
+import Header from './components/Header';
 
 const App = () => {
   const [isLightMode, setIsLightMode] = useState<boolean>(false);
 
   return (
     <div className={`container ${isLightMode ? 'lightMode' : ''}`}>
-      <header>
-        <h1>Elite Assistant</h1>
-        <h4>Coming Soon</h4>
-        <img src={elitelogo} className='eliteLogo' alt='Elite Logo' />
-      </header>
+      <Header />
     </div>
   );
 }
