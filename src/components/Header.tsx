71a884ef0e6cff19e56f6ce6f24f399e16ac8b40
Rubeun Tan <github@rubeun.com>
2025-03-15 16:28:18 -0700

    add Header component and styles

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import styles from './Header.module.css';
+import elitelogo from '../logo-elite1.svg';
+
+const Header = () => {
+  return ( 
+    <header className={`${styles.header} `}>
+      <h1>Elite Assistant</h1>
+      <h4>Coming Soon</h4>
+      <img src={elitelogo} className='eliteLogo' alt='Elite Logo' />
+    </header>
+);
+}
+
+export default Header;
