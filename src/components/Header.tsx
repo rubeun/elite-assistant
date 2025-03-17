c0d99d1a237309b833fc92e51b4bf3c5c1f2ccac
Rubeun Tan <github@rubeun.com>
2025-03-16 21:17:06 -0700

    Header cleanup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,9 @@ const Header = () => {
   return ( 
     <header className={`${styles.header} `}>
       <h1>Elite Assistant</h1>
-      <h4>Coming Soon</h4>
       <img src={elitelogo} className={styles.eliteLogo} alt='Elite Logo' />
     </header>
-);
+  );
 }
 
 export default Header;
