94ab4885190e612690194384e5e1501a0ced56df
Rubeun Tan <github@rubeun.com>
2025-03-18 21:51:11 -0700

    add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return ( 
+    <footer className={`${styles.footer} `}>
+      <div>Created by Rubeun</div>
+    </footer>
+  );
+}
+
+export default Footer;
