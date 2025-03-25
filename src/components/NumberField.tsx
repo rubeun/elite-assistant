da0795cc5ed1c169f25159e4d9cf746c05038b9c
Rubeun Tan <github@rubeun.com>
2025-03-25 13:51:38 -0700

    add NumberField component to edit numbers

---
+++ src/components/NumberField.tsx
@@ -0,0 +1,38 @@
+import { useRef, useState, MouseEvent } from "react";
+import styles from './NumberField.module.css';
+
+type NumberFieldType = {
+  numValue: number,
+  amountReached: boolean,
+  updateAmount: any,
+}
+
+
+const NumberField = ({numValue, amountReached, updateAmount}: NumberFieldType) => {
+  const [value, setValue] = useState<number | undefined>(numValue);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleEdit = (event: MouseEvent<HTMLInputElement>) => {
+    event.currentTarget.select();
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value === '' ? undefined : parseFloat(e.target.value);
+    setValue(newValue);
+    updateAmount(newValue);
+  }
+
+  return (
+    <div className={`${styles.numfieldContainer} ${amountReached ? styles.noRemaining : ''}`}>
+      <input 
+        ref={inputRef} 
+        type="number" 
+        value={value === undefined ? '' : value}
+        onClick={handleEdit}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
+
+export default NumberField;
